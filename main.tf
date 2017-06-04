ebede6e06b5792beced917da2b018d484c5182c6
ConvectionTech <manoranjanb@gmail.com>
2017-06-04 17:28:07 -0400

    terraform codes

--- main.tf
+++ main.tf
@@ -5,7 +5,23 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
- tags {
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World.This is Debarchana....how is everything at your end" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+  tags {
   Name = "myfirstterainstance"
  }
+}
+resource "aws_security_group" "instance" {
+  name = "first-terraform-instance"
+
+ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
